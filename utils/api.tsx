51867f1b0fb278cafd234322800f48a93d4e85ab
ShoyoNo10 <flyingshoyo10@gmail.com>
2025-08-08 16:18:56 +0800

    api changes

--- utils/api.tsx
+++ utils/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_SERVER_URI || 'http://localhost:5000/',
+  baseURL: process.env.NEXT_PUBLIC_SERVER_URI,
 });
 
 export default API;
